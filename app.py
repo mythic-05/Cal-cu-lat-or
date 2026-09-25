97bd4c978e5a5d2bf3346c7faf056816165eb3c7
mythic-05 <aayan.2436@beaconite.edu.pk>
2026-09-25 23:18:53 +0500

    Fix formatting issues in app.py

--- app.py
+++ app.py
@@ -449,10 +449,10 @@ else:
 
     cleaned_mode = app_mode.replace("🕹️Tetris", "🕹️Tetris").strip()
     st.title(app_mode)
-    components.html(games[cleaned_mode], height=560)
+    components.html(games[cleaned_mode], height=560) 
 
     
-"🍕 Pac-Man": """
+  "🍕 Pac-Man": """
             <style>
                 canvas { background: #000; display: block; margin: auto; border: 4px solid #fff; } 
                 h1, p { color: white; text-align: center; font-family: sans-serif; }
